7431a722df998428f6ecfecfa8240c9780d28bef
John Harison <john.harison99@gmail.com>
2019-05-07 11:09:03 +0700

    sadasd

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var conn = mysql.createConnection({
   host : "srv64.niagahoster.com",
-  user : "semj2564_pbk0k",
+  user : "semj2564_pbkk",
   password : "j0hnpr@s",
   database : "semj2564_pbkk"
 });
