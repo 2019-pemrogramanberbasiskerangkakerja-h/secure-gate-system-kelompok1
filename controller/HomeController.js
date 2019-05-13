b7a957abb39bfeef6f0dca2d703df9d021cb847c
John Harison <john.harison99@gmail.com>
2019-05-13 14:07:27 +0700

    fix api

--- controller/HomeController.js
+++ controller/HomeController.js
@@ -85,76 +85,85 @@ exports.login=  (req,res)=>{
 
 exports.apilogin=(req,res)=>{
   var gate = req.body.gate;
-  var nrp = req.body.username;
+  var nrp = req.body.nrp;
   var password = req.body.pass;
 
-  conn.query("SELECT * from users where users.nrp=? AND users.password=?",[nrp,password],(err,rows,fields)=>{
-    if(rows.length==0){ //kalau salah username / password
-      console.log("username / password salah")
-      res.status(401);
-      res.setHeader('Content-Type','application/json');
-      res.send(JSON.stringify({'error' : 'wrong user/pass'}));
-      return;
-    }
-    else{
-      var id_user = rows[0].id_users;
+  if(!gate && !nrp && password){
+    res.status(404);
+    res.setHeader('Content-Type','application/json');
+    res.send(JSON.stringify({'error' : 'Format body (gate,nrp,pass)'}));
+    return;
+  }
+  else{
+    conn.query("SELECT * from users where users.nrp=? AND users.password=?",[nrp,password],(err,rows,fields)=>{
+      if(rows.length==0){ //kalau salah username / password
+        console.log("username / password salah")
+        res.status(401);
+        res.setHeader('Content-Type','application/json');
+        res.send(JSON.stringify({'error' : 'wrong user/pass'}));
+        return;
+      }
+      else{
+        var id_user = rows[0].id_users;
 
-      conn.query("INSERT INTO log(name_log,id_users,id_gate) VALUES(?,?,?)",["try login",id_user,gate],(err,rows,fields)=>{ //bikin log
-        if(err) console.log(err);
-        conn.query("SELECT * FROM access where id_users=? and id_gate=?",[id_user,gate],(err,rows,fields)=>{ //check access
+        conn.query("INSERT INTO log(name_log,id_users,id_gate) VALUES(?,?,?)",["try login",id_user,gate],(err,rows,fields)=>{ //bikin log
           if(err) console.log(err);
-          if(rows.length==0){ //kalau ga punya access
-            // console.log("masuk");
-            conn.query("INSERT INTO log(name_log,id_users,id_gate) VALUES(?,?,?)",["no access",id_user,gate],(err,rows,fields)=>{
-              if(err) console.log(err);
-            });
-            console.log("Tidak ada access");
-            res.status(401);
-            res.setHeader('Content-Type','application/json');
-            res.send(JSON.stringify({'error' : 'No Access'}))
-            return;
-          }
-          else{ // punya access
-            conn.query("SELECT * from gate where id_gate=?",[gate],(err,rows,fields)=>{
-                if(rows.length==1){
-                  console.log(rows);
-                  var jam_buka = rows[0].jam_buka.split(/[:]/);
-                  var jam_tutup = rows[0].jam_tutup.split(/[:]/);
-                  var date = new Date();
-                  var today = new Date().toLocaleString("en-US",{timeZone:"Asia/Jakarta"});
-                  var start = new Date(date.getFullYear(),date.getMonth(),date.getDate(),jam_buka[0],jam_buka[1],jam_buka[2]).toLocaleString("en-US",{timeZone:"Asia/Jakarta"});
-                  var end = new Date(date.getFullYear(),date.getMonth(),date.getDate(),jam_tutup[0],jam_tutup[1],jam_tutup[2]).toLocaleString("en-US",{timeZone:"Asia/Jakarta"});
-
-                  if(Date.parse(start) < Date.parse(today) && Date.parse(today) < Date.parse(end)){
-                    conn.query("INSERT INTO log(name_log,id_users,id_gate) VALUES(?,?,?)",["login berhasil",rows[0].id_user,gate],(err,rows,fields)=>{
-                      if(err) console.log(err);
-                      req.session.nrp = nrp;
-                      req.session.gate = gate;
-                      console.log(req.session.nrp);
-                      res.status(200);
-                      res.setHeader('Content-Type','application/json');
-                      res.send(JSON.stringify({'status' : 'Login Success'}));
-                      return;
-                    });
-                  }
-
-                  else{ //kalau gate belum buka
-                    conn.query("INSERT INTO log(name_log,id_users,id_gate) VALUES(?,?,?)",["gate not open yet",rows[0].id_user,gate],(err,rows,fields)=>{
-                      if(err) console.log(err);
-                      console.log("Gate belum buka");
-                      res.status(401);
-                      res.setHeader('Content-Type','application/json');
-                      res.send(JSON.stringify({'error' : 'Gate not Open Yet'}));
-                    })
-                  }
-                }
+          conn.query("SELECT * FROM access where id_users=? and id_gate=?",[id_user,gate],(err,rows,fields)=>{ //check access
+            if(err) console.log(err);
+            if(rows.length==0){ //kalau ga punya access
+              // console.log("masuk");
+              conn.query("INSERT INTO log(name_log,id_users,id_gate) VALUES(?,?,?)",["no access",id_user,gate],(err,rows,fields)=>{
+                if(err) console.log(err);
               });
-          }
+              console.log("Tidak ada access");
+              res.status(401);
+              res.setHeader('Content-Type','application/json');
+              res.send(JSON.stringify({'error' : 'No Access'}))
+              return;
+            }
+            else{ // punya access
+              conn.query("SELECT * from gate where id_gate=?",[gate],(err,rows,fields)=>{
+                  if(rows.length==1){
+                    console.log(rows);
+                    var jam_buka = rows[0].jam_buka.split(/[:]/);
+                    var jam_tutup = rows[0].jam_tutup.split(/[:]/);
+                    var date = new Date();
+                    var today = new Date().toLocaleString("en-US",{timeZone:"Asia/Jakarta"});
+                    var start = new Date(date.getFullYear(),date.getMonth(),date.getDate(),jam_buka[0],jam_buka[1],jam_buka[2]).toLocaleString("en-US",{timeZone:"Asia/Jakarta"});
+                    var end = new Date(date.getFullYear(),date.getMonth(),date.getDate(),jam_tutup[0],jam_tutup[1],jam_tutup[2]).toLocaleString("en-US",{timeZone:"Asia/Jakarta"});
 
+                    if(Date.parse(start) < Date.parse(today) && Date.parse(today) < Date.parse(end)){
+                      conn.query("INSERT INTO log(name_log,id_users,id_gate) VALUES(?,?,?)",["login berhasil",rows[0].id_user,gate],(err,rows,fields)=>{
+                        if(err) console.log(err);
+                        req.session.nrp = nrp;
+                        req.session.gate = gate;
+                        console.log(req.session.nrp);
+                        res.status(200);
+                        res.setHeader('Content-Type','application/json');
+                        res.send(JSON.stringify({'status' : 'Login Success'}));
+                        return;
+                      });
+                    }
+
+                    else{ //kalau gate belum buka
+                      conn.query("INSERT INTO log(name_log,id_users,id_gate) VALUES(?,?,?)",["gate not open yet",rows[0].id_user,gate],(err,rows,fields)=>{
+                        if(err) console.log(err);
+                        console.log("Gate belum buka");
+                        res.status(401);
+                        res.setHeader('Content-Type','application/json');
+                        res.send(JSON.stringify({'error' : 'Gate not Open Yet'}));
+                      })
+                    }
+                  }
+                });
+            }
+
+          });
         });
-      });
-    }
-  });
+      }
+    });
+  }
+
 }
 
 exports.adduser=(req,res)=>{
