73d38b02f95ed599c6f474d332e5a6e82534540b
John Harison <john.harison99@gmail.com>
2019-05-22 23:11:07 +0700

    coba lg

--- database/db.js
+++ database/db.js
@@ -1,15 +1,32 @@
 var mysql = require('mysql');
 
-var conn = mysql.createPool({
+var conn_config = {
   host : "srv64.niagahoster.com",
   user : "semj2564_pbkk",
   password : "j0hnpr@s",
   database : "semj2564_pbkk"
-});
+};
+var conn= mysql.createConnection(conn_config);
 
-conn.getConnection((err,connection)=>{
-  if(err) throw err;
-  connection.release();
-});
+function handleDC(){
+    conn = mysql.createConnection(conn_config);
+
+    conn.connect((err)=>{
+      if(err){
+        setTimeout(handleDC,2000);
+      }
+    });
+
+    conn.on('error',(err)=>{
+      if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+        handleDC();
+      }
+      else{
+        throw err;
+      }
+    });
+}
+
+handleDC();
 
 module.exports=conn;
