d8b875c4279f3ccc43c2b61caaaf74864606eb8d
John Harison <john.harison99@gmail.com>
2019-05-22 23:05:12 +0700

    try

--- database/db.js
+++ database/db.js
@@ -4,12 +4,12 @@ var conn = mysql.createPool({
   host : "srv64.niagahoster.com",
   user : "semj2564_pbkk",
   password : "j0hnpr@s",
-  database : "semj2564_pbkk",
-  connectionLimit : 20
+  database : "semj2564_pbkk"
 });
 
-conn.connect((err)=>{
+conn.getConnection((err,connection)=>{
   if(err) throw err;
+  connection.release();
 });
 
 module.exports=conn;
