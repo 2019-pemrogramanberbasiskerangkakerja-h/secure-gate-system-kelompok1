d4dc2d54b83804355fc8f3df2898f4c316fef55f
John Harison <john.harison99@gmail.com>
2019-05-22 22:42:26 +0700

    ok

--- database/db.js
+++ database/db.js
@@ -1,14 +1,32 @@
 var mysql = require('mysql');
 
-var conn = mysql.createConnection({
+var conn = {
   host : "srv64.niagahoster.com",
   user : "semj2564_pbkk",
   password : "j0hnpr@s",
   database : "semj2564_pbkk"
-});
+};
+var connection;
 
-conn.connect((err)=>{
-  if(err) throw err;
-});
+function handleDC(){
+    connection = mysql.createConnection(conn);
+
+    connection.connect((err)=>{
+      if(err){
+        setTimeout(handleDC,2000);
+      }
+    });
+
+    connection.on('error',(err)=>{
+      if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+        handleDC();
+      }
+      else{
+        throw err;
+      }
+    });
+}
+
+handleDC();
 
 module.exports=conn;
